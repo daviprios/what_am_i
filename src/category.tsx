41bfd264b70b9f6b121e653b34465e54386cfacc
Davi Paim Rios <davipaimrios@gmail.com>
2021-12-28 22:18:52 -0300

    Add Home Style

--- src/category.tsx
+++ src/category.tsx
@@ -2,15 +2,15 @@ import React from 'react';
 
 import {useNavigation} from '@react-navigation/core';
 import {StackNavigationProp} from '@react-navigation/stack';
-import {Button, View} from 'react-native';
+import {Pressable, StyleSheet, Text} from 'react-native';
 
 import {StackParamList} from './App';
 import CategoryManager from './categoryManager';
 
 type CategoryButtonProp = StackNavigationProp<StackParamList, 'Home'>;
 
-const Category = (props: {name: string}) => {
-  const {name} = props;
+const Category = (props: {name: string; backgroundColor: string}) => {
+  const {name, backgroundColor} = props;
 
   const navigation = useNavigation<CategoryButtonProp>();
 
@@ -18,21 +18,40 @@ const Category = (props: {name: string}) => {
   const categoryValues = categoryManager.getCategory(name);
 
   return (
-    <View>
-      <Button
-        title={name}
-        onPress={() =>
-          navigation.navigate({
-            name: 'Game',
-            params: {categoryValues},
-            merge: true,
-          })
-        }
-      />
-    </View>
+    <Pressable
+      style={{...styles.button, backgroundColor}}
+      onPress={() =>
+        navigation.navigate({
+          name: 'Game',
+          params: {categoryValues},
+          merge: true,
+        })
+      }>
+      <Text style={styles.text}>{name}</Text>
+    </Pressable>
   );
 };
 
 export default Category;
 
-//const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  button: {
+    height: 150,
+    width: '50%',
+  },
+  text: {
+    height: '100%',
+    width: '100%',
+
+    textAlign: 'center',
+    textAlignVertical: 'center',
+
+    color: 'white',
+    fontFamily: 'sans-serif',
+    fontSize: 20,
+
+    textShadowColor: 'black',
+    textShadowOffset: {width: 1, height: 1},
+    textShadowRadius: 10,
+  },
+});
--- src/home.tsx
+++ src/home.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {ScrollView, Text, View} from 'react-native';
+import {ScrollView, StyleSheet, Text, View} from 'react-native';
 
 import CategoryManager from './categoryManager';
 import Category from './category';
@@ -14,17 +14,63 @@ const Home = () => {
   }
 
   return (
-    <View>
-      <Text>Quem sou eu?</Text>
-      <ScrollView>
-        {categoriesName.map(name => {
-          return <Category key={name} name={name} />;
+    <View style={styles.container}>
+      <Text style={styles.header}>QUEM SOU EU?</Text>
+      <ScrollView
+        style={styles.scrolled}
+        contentContainerStyle={styles.scrolledContainer}>
+        {categoriesName.map((name, index) => {
+          const factor = index % 8;
+          return (
+            <Category
+              key={name}
+              name={name}
+              backgroundColor={`hsl(${factor * 45}, 80%, 80%)`}
+            />
+          );
         })}
       </ScrollView>
     </View>
   );
 };
 
-//const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    alignItems: 'center',
+
+    height: '100%',
+    width: '100%',
+  },
+  header: {
+    textAlign: 'center',
+    textAlignVertical: 'center',
+
+    height: 50,
+    width: '100%',
+
+    color: 'white',
+    backgroundColor: 'hsl(235, 80%, 60%)',
+
+    fontWeight: 'bold',
+    fontSize: 25,
+    fontFamily: 'serif',
+  },
+  scrolled: {
+    height: '100%',
+    width: '100%',
+
+    backgroundColor: 'white',
+  },
+  scrolledContainer: {
+    display: 'flex',
+
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+
+    height: '100%',
+    width: '100%',
+  },
+});
 
 export default Home;
